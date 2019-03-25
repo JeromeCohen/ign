5045fff0fd557805d3fba275ddd7b2f5fb348280
jeromecohen <jeromecohen98@gmail.com>
2019-03-24 21:53:07 -0400

    created SQL tables

--- back-end/data/script.py
+++ back-end/data/script.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import uuid
+import sqlalchemy as db
 
 # imort csv into master datafrsame and drop unneccessary columns and duplicates
 df = pd.read_csv('codefoo.csv')
@@ -10,7 +11,6 @@ columnsToDrop = ['thumbnail_1_size', 'thumbnail_1_width', 'thumbnail_1_height',
                  'state', 'Unnamed: 0', 'description.1']
 df.drop(columns=columnsToDrop, inplace=True)
 df = df.apply(lambda x: x.astype(str).str.strip())
-
 
 # create tags table and unique identifiers
 tag_1_df = df[df['tag_1'] != 'NaN'][['tag_1', 'content_id']].rename(columns={'tag_1':'tag'})
@@ -23,9 +23,8 @@ intermediate_df = tags_df
 
 #creates df of all exisiting tags with id, and all edges between content and tags
 tags_df = tags_df.drop_duplicates(subset='tag').drop(columns=['content_id'])
-tags_df['tag_id'] = [uuid.uuid4() for x in range(len(tags_df))]
+tags_df['tag_id'] = [uuid.uuid4().hex for x in range(len(tags_df))]
 tag_relations_df = tags_df.merge(intermediate_df, on='tag')[['content_id', 'tag_id']]
-
 
 # - Drop columns that are not relevant to articles and visa versa
 articles_df = df[df['content_type'] == 'article']
@@ -36,7 +35,6 @@ videos_df = df[df['content_type'] == 'video']
 videos_df.drop(columns=['headline', 'content_type', 'author_1', 'author_2'],
                inplace=True)
 
-
 # Create two tables to store author data
 authors_df = articles_df[['content_id', 'author_1']].rename(columns={'author_1' : 'author'})
 authors_df['order'] = 1
@@ -44,9 +42,15 @@ authors_2_df = articles_df[articles_df['author_2'] != 'nan'][['content_id', 'aut
 authors_2_df['order'] = 2
 authors_df = authors_df.append(authors_2_df)
 
-authors_df['author_id'] = [uuid.uuid4() for x in range(len(authors_df))]
+authors_df['author_id'] = [uuid.uuid4().hex for x in range(len(authors_df))]
 author_names_df = authors_df.rename(columns={'author':'name'}).drop(columns=['content_id', 'order'])
 authors_df.drop(columns=['author'], inplace=True)
 
-print(tags_df)
-print(tag_relations_df)
+############# - Pushing Pandas DFs to MySQL - #############
+engine = db.create_engine('mysql+pymysql://ba46ee82f0feb2:c02c1f42@us-cdbr-iron-east-03.cleardb.net/heroku_7e1867d4621a886')
+articles_df.to_sql('articles', engine, if_exists='replace', index=False)
+videos_df.to_sql('videos', engine, if_exists='replace', index=False)
+tags_df.to_sql('tags', engine, if_exists='replace', index=False)
+tag_relations_df.to_sql('tag_relations', engine, if_exists='replace', index=False)
+authors_df.to_sql('author_relations', engine, if_exists='replace', index=False)
+author_names_df.to_sql('authors', engine, if_exists='replace', index=False)
